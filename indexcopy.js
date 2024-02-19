bd68dd260d00ebaf55bf21714f43c099d5317e80
Santhosh Bodla <santhosh.bodla.16cse@bml.edu.in>
2024-02-19 14:17:32 -0500

    files added

--- indexcopy.js
+++ indexcopy.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const API_KEY = "EAAYZCYomW2gcBOyEJnJX0s347kNcoJsiKZAZAiff7StipurdND2uebwm1sx8ux1BqhAK3ojGD4kD8rdr249TKKWlnHeufr0DlNWQMAFHZCbokvDwlBZC9UD5I6zH0XGtArMY84zzMuhGGqZBVhdd4TLhO6QHKHzuIvrqxoGRJ7KcZAvZCGRG1uaFFi7nVeRy6kT8AHzs5FwDUvZBZCgnJitUx44N01E9iCZCCrqPPUZD"
+const token = "EAAYZCYomW2gcBO0ZAsidy28UQd9fcPjhjQmXReozQKMZAonLIq52Aaguq95WSlvQz46B0nmQiBVIVA4smsF5FDR6821kHpd0DS9S9eHzUJv3EcIZBXbeEPTSGtOLqzHRAuvIGC1RJOIiGllA8JMHgEz1QW5QOQ6oHNseT8UFEZCJRvxKCcc6qYybzk2MdZAcLNB73fCwA3QbaqUsf9TLXSiYmWXbV0KbPwZBDMZD"
 const PHONE_NUMBER = '+14082032469';
 
 const app = express();
@@ -13,35 +13,68 @@ app.get("/",(req,res)=>{
 
   res.send('<h1>Hi This is santhosh sadwdw</h1>')
 })
+//to verify the callback url from dashboard side - cloud api side
+app.get("/webhook",(req,res)=>{
+  let mode=req.query["hub.mode"];
+  let challange=req.query["hub.challenge"];
+  let token=req.query["hub.verify_token"];
 
-app.post('/webhook', async (req, res) => {
-  // const { body } = req;
-  // const { from, message } = body;
 
-  console.log("enteredddddddddddddddddd")
+   if(mode && token){
 
-  // Customize this logic to send different responses based on the incoming message
-  // const responseMessage = `You said: ${message}`;
+       if(mode==="subscribe" && token===mytoken){
+           res.status(200).send(challange);
+       }else{
+           res.status(403);
+       }
 
-  // Send the response message back to the sender
-  // await axios.post('https://graph.facebook.com/v18.0/226820867186026/messages', {
-  //   to: from,
-  //   messaging_product: 'whatsapp',
-  //   type: 'template',
-  //   template: JSON.stringify({
-  //     name: responseMessage,
-  //     language: {
-  //         code: "en_US"
-  //     }
-  //   }),
-  // }, {
-  //   headers: {
-  //     'Authorization': `Bearer ${API_KEY}`,
-  //     'Content-Type': 'application/json',
-  //   },
-  // });
+   }
 
-  res.sendStatus(200);
+});
+app.post("/webhook",(req,res)=>{ //i want some 
+
+  let body_param=req.body;
+
+  console.log(JSON.stringify(body_param,null,2));
+
+  if(body_param.object){
+      console.log("inside body param");
+      if(body_param.entry && 
+          body_param.entry[0].changes && 
+          body_param.entry[0].changes[0].value.messages && 
+          body_param.entry[0].changes[0].value.messages[0]  
+          ){
+             let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+             let from = body_param.entry[0].changes[0].value.messages[0].from; 
+             let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+             console.log("phone number "+phon_no_id);
+             console.log("from "+from);
+             console.log("boady param "+msg_body);
+
+             axios({
+                 method:"POST",
+                 url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                 data:{
+                     messaging_product:"whatsapp",
+                     to:from,
+                     text:{
+                         body:"Hi.. I'm Prasath, your message is "+msg_body
+                     }
+                 },
+                 headers:{
+                     "Content-Type":"application/json"
+                 }
+
+             });
+
+             res.sendStatus(200);
+          }else{
+              res.sendStatus(404);
+          }
+
+  }
+
 });
 
 app.listen(PORT, () => {
